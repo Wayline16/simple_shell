50db34008308605c7510dc22f5525002f5794018
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-10-09 08:58:30 +0200

    custom tokenizer

--- get_args.c
+++ get_args.c
@@ -7,26 +7,60 @@
  *
  *	Return: array of arguments.
  */
-char **get_args(char *line)
-{
-     char *tokens = NULL;
-     char *delim = " \n";
-     char **args = NULL;
-     int i = 0;
-     int size_tokens;
+char **get_args(char *line) {
+    const char *delim = " \n";
+    char **args = NULL;
+    int size_tokens = 0;
+    int i = 0;
+    
+    char *token = custom_strtok(line, delim);
+    
+    while (token != NULL) {
+        size_tokens++;
+        token = custom_strtok(NULL, delim);
+    }
+    
+    args = malloc(sizeof(char *) * (size_tokens + 1));
+    if (args == NULL) {
+        return NULL;
+    }
+    
+    token = custom_strtok(line, delim);
+    
+    while (token != NULL) {
+        
+        args[i] = malloc(strlen(token) + 1);
+        if (args[i] == NULL) {
+            
+            return NULL;
+        }
+       
+        strcpy(args[i], token);
+        
+        i++;
+        token = custom_strtok(NULL, delim);
+    }
+    
+    args[i] = NULL;
+    
+    return args;
+}
 
-     size_tokens = _count_tokens(line, delim);
-     args = malloc(sizeof(char *) * (size_tokens));
-     if (args == NULL)
-          return (NULL);
-     tokens = strtok(line, delim);
-     if (tokens == NULL)
-          return (NULL);
-     while (tokens != NULL)
-     {
-          args[i++] = tokens;
-          tokens = strtok(NULL, delim);
-     }
-     args[i] = NULL;
-     return (args);
+
+size_t strlen(const char *str) {
+    size_t len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+
+char *strcpy(char *dest, const char *src) {
+    char *dest_start = dest;
+    while (*src != '\0') {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+    return dest_start;
 }

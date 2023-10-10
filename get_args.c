bc0b5ff4bed128d05b28b62c35d4600e6f5153f7
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-10-10 07:55:58 +0200

    Updated get_args.c

--- get_args.c
+++ get_args.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "main.h"
 
-char **get_args(char *line)
+/*char **get_args(char *line)
 {
 
     char *delim = " \n";
@@ -38,4 +38,28 @@ char **get_args(char *line)
     args[i] = NULL;
 
     return args;
+}*/
+
+char **get_args(char *line)
+{
+     char *tokens = NULL;
+     char *delim = " \n";
+     char **args = NULL;
+     int i = 0;
+     int size_tokens;
+
+     size_tokens = _count_tokens(line, delim);
+     args = malloc(sizeof(char *) * (size_tokens));
+     if (args == NULL)
+          return (NULL);
+     tokens = custom_strtok(line, delim);
+     if (tokens == NULL)
+          return (NULL);
+     while (tokens != NULL)
+     {
+          args[i++] = tokens;
+          tokens = custom_strtok(NULL, delim);
+     }
+     args[i] = NULL;
+     return (args);
 }
